4824cdb7c3ffa0c5d14ef9ec5b62b8be7df5122f
manuel cretone <manuel.cretone@studenti.unicam.it>
2019-11-22 11:20:08 +0100

    added other states

--- frontend/src/app/models/CountriesInfo.ts
+++ frontend/src/app/models/CountriesInfo.ts
@@ -1,17 +1,37 @@
 import { CountryInfo } from './CountryInfo.js';
-import * as italy from "./../countries/italy.json";
+import * as italy from './../countries/italy.json';
 import * as france from './../countries/france.json';
+import * as netherlands from './../countries/netherlands.json';
+import * as denmark from './../countries/denmark.json';
+import * as portugal from './../countries/portugal.json';
+import * as bulgaria from './../countries/bulgaria.json';
+import * as cyprus from './../countries/cyprus.json';
+import * as czechrepublic from './../countries/czechrepublic.json';
+import * as hungary from './../countries/hungary.json';
+import * as luxembourg from './../countries/luxembourg.json';
+import * as austria from './../countries/austria.json';
+const COUNTRIES = [
+  italy,
+  france,
+  netherlands,
+  denmark,
+  portugal,
+  bulgaria,
+  cyprus,
+  czechrepublic,
+  hungary,
+  luxembourg,
+  austria
+];
 
-const COUNTRIES = [italy, france];
+const dictionary: { [name: string]: string } = {};
 
-let dictionary: {[name: string]: string} = {}
-
-for(let i = 0; i < COUNTRIES.length; i++){
-    let country: any = COUNTRIES[i];
-    country = country.default;
-    for(let j = 0; j < country.length; j++){
-        dictionary[country[j].name] = country[j].chart_name
-    }
+for (let i = 0; i < COUNTRIES.length; i++) {
+  let country: any = COUNTRIES[i];
+  country = country.default;
+  for (let j = 0; j < country.length; j++) {
+    dictionary[country[j].name] = country[j].chart_name;
+  }
 }
 
 export default dictionary;
